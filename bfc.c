de2b8ee50449867fe345b0031a2faf69a725b20f
Samuel <samuel.d.lewis@wmich.edu>
2016-12-05 18:17:29 -0500

    added pretesting to loops

--- bfc.c
+++ bfc.c
@@ -74,7 +74,7 @@ void bf_to_asm(asm_h* lines, char* commands){
 	// the pointer which moves around with '<' and '>' is buf_p
 
 	// buf_p is generally in eax
-	buf_write(lines, "mov eax, buf_p\n");
+	buf_write(lines, "mov eax, buf_p");
 	// buf is generally in ecx
 	buf_write(lines, "mov ecx, buf\n");
 	for (i = 0; i < strlen(commands); i++){
@@ -93,15 +93,15 @@ void bf_to_asm(asm_h* lines, char* commands){
 			// if we have more than one
 			if (a_count > 1){
 				// put that many in edx
-				sprintf(temp, "mov edx, %d\n", a_count);
+				sprintf(temp, "mov edx, %d", a_count);
 				buf_write(lines, temp);
 				// add them to the current cell
-				buf_write(lines, "add [ecx], edx\n\n");
+				buf_write(lines, "add [ecx], edx\n");
 			// we only have one '+'
 			} else {
 				// add only 1 to the current cell
-				buf_write(lines, "mov ebx, 1\n");
-				buf_write(lines, "add [ecx], ebx\n\n");
+				buf_write(lines, "mov ebx, 1");
+				buf_write(lines, "add [ecx], ebx\n");
 			}
 		// logic for '-' command
 		// this command subtracts one from the current cell
@@ -114,81 +114,89 @@ void bf_to_asm(asm_h* lines, char* commands){
 			i--;
 
 			if (a_count > 1){
-				sprintf(temp, "mov edx, %d\n", a_count);
+				sprintf(temp, "mov edx, %d", a_count);
 				buf_write(lines, temp);
 				// subtract that many from the current cell
-				buf_write(lines, "sub [ecx], edx\n\n");
+				buf_write(lines, "sub [ecx], edx\n");
 			} else {
-				buf_write(lines, "mov ebx, 1\n");
+				buf_write(lines, "mov ebx, 1");
 				// subtract from the current cell
-				buf_write(lines, "sub [ecx], ebx\n\n");
+				buf_write(lines, "sub [ecx], ebx\n");
 			}
 		// logic for '.' command
 		// this command prints out the ascii character for the current cell
 		} else if (commands[i] == '.'){
 			// put 4 in eax, for sys_write
-			buf_write(lines, "mov eax, 4\n");
+			buf_write(lines, "mov eax, 4");
 			// put 1 in ebx, for stdout
-			buf_write(lines, "mov ebx, 1\n");
+			buf_write(lines, "mov ebx, 1");
 			// put 1 in edx, for length 1 character array (1 character)
-			buf_write(lines, "mov edx, 1\n");
+			buf_write(lines, "mov edx, 1");
 			// call sys_write
 			// (buf pointer is already in ecx)
-			buf_write(lines, "int 0x80\n");
+			buf_write(lines, "int 0x80");
 			// put buf_p back in eax, as that is where its expected to be for other commands
-			buf_write(lines, "mov eax, buf_p\n");
+			buf_write(lines, "mov eax, buf_p");
 			// put buf back in ecx, as taht is where its expected to be for other commands
-			buf_write(lines, "mov ecx, buf\n");
+			buf_write(lines, "mov ecx, buf");
 			// put ecx at the correct index
-			buf_write(lines, "add ecx, [buf_p]\n\n");
+			buf_write(lines, "add ecx, [buf_p]\n");
 		// logic for '>' command
 		// this command moves the pointer 1 cell to the right
 		} else if (commands[i] == '>'){
 			// put 4 in ebx, as cells are 4 bytes wide
-			buf_write(lines, "mov ebx, 4\n");
+			buf_write(lines, "mov ebx, 4");
 			// add this to buf_p
-			buf_write(lines, "add [buf_p], ebx\n");
+			buf_write(lines, "add [buf_p], ebx");
 			// reset ecx location
-			buf_write(lines, "mov ecx, buf\n");
+			buf_write(lines, "mov ecx, buf");
 			// add the new pointer to ecx
-			buf_write(lines, "add ecx, [buf_p]\n\n");
+			buf_write(lines, "add ecx, [buf_p]\n");
 		// logic for '<' command
 		// this command moves the pointer 1 cell to the left
 		// refer to comments to the '+' command
 		} else if (commands[i] == '<'){
-			buf_write(lines, "mov ebx, 4\n");
+			buf_write(lines, "mov ebx, 4");
 			// subtract this from buf_p
-			buf_write(lines, "sub [buf_p], ebx\n");
-			buf_write(lines, "mov ecx, buf\n");
-			buf_write(lines, "add ecx, [buf_p]\n\n");
+			buf_write(lines, "sub [buf_p], ebx");
+			buf_write(lines, "mov ecx, buf");
+			buf_write(lines, "add ecx, [buf_p]\n");
 		// logic for '[' command
 		// this command starts a loop 
 		} else if (commands[i] == '['){
 			// create the label for the loop
-			sprintf(temp, "ll%ds%d:\n", label_l, labels_d[label_l]);
-			// manage label variables
-			label_l++;
+			buf_write(lines, "mov edx, [ecx]");
+			// compare with 0
+			buf_write(lines, "cmp edx, 0");
+			sprintf(temp, "jz el%ds%d", label_l, labels_d[label_l]);
+			buf_write(lines, temp);
+			sprintf(temp, "ll%ds%d:", label_l, labels_d[label_l]);
 			// put the label in assembly
 			buf_write(lines, temp);
+			// manage label variables
+			label_l++;
 		// logic for ']' command
 		// this command marks the end of a loop
 		// the current cell will be comapred with 0, and repeat if it is not
 		} else if (commands[i] == ']'){
 			// put the value at the current cell into edx
-			buf_write(lines, "mov edx, [ecx]\n");
+			buf_write(lines, "mov edx, [ecx]");
 			// compare with 0
-			buf_write(lines, "cmp edx, 0\n");
+			buf_write(lines, "cmp edx, 0");
 			// jump to corresponding label if they are not equal
-			sprintf(temp, "jnz ll%ds%d\n", label_l-1, labels_d[label_l-1]);
+			sprintf(temp, "jnz ll%ds%d", label_l-1, labels_d[label_l-1]);
 			// add jump command to assembly
 			buf_write(lines, temp);
 			// manage label variables
 			// go down one level
 			label_l--;
+			sprintf(temp, "el%ds%d:", label_l, labels_d[label_l]);
+			buf_write(lines, temp);
 			// one less label on this level in progress
 			// label is now done
 			// add to completed labels
 			labels_d[label_l]++;
+
 		}
 	}
 }
